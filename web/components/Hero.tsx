2333b4ae26537db5abe6ecf56a4736396caa64ed
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-10-21 18:17:42 +0200

    hero mobile improvements

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -3,16 +3,18 @@ import { useTranslation } from 'next-i18next';
 export default function Hero() {
 	const { t } = useTranslation('common');
 
-	const videoDate = 11; // Replace with your dynamic date value
-	const videoLength = 27; // Replace with your dynamic video length
+	const videoDate = 11;
+	const videoLength = 27;
 
 	return (
 		<div className="grid grid-cols-1 items-center md:grid-cols-2">
-			<div className="md:ml-[200px] pl-24 text-customGray dark:text-customWhite">
+			<div className="pl-6 m-5 md:ml-[200px] md:pl-24 text-customGray dark:text-customWhite">
 				<div className="uppercase font-bold mb-2">
 					{t('subheading')}
 				</div>
-				<h1 className="text-6xl font-bold mb-5">{t('h1')}</h1>
+				<h1 className="text-4xl md:text-6xl font-bold mb-5">
+					{t('h1')}
+				</h1>
 				<p className="text-lg mb-8 md:mr-10">{t('description')}</p>
 				<a href="/glossary">
 					<button className="items-center border border-transparent rounded inline-flex font-semibold justify-center whitespace-nowrap focus:outline-none text-base h-12 leading-6 px-4 bg-customBitcoin text-white dark:bg-customWhite dark:text-customDark">
@@ -20,15 +22,14 @@ export default function Hero() {
 					</button>
 				</a>
 			</div>
-			<div className="p-16 pl-10 bg-white dark:bg-customGray">
+			<div className="p-4 md:p-16 pl-10 bg-white dark:bg-customGray">
 				<div>
 					<div className="uppercase font-bold mb-2">
 						{t('featured')}
 					</div>
-					<div className="mb-4">
+					<div className="mb-4 relative rounded-lg overflow-hidden">
 						<iframe
-							className="rounded-lg w-[auto] md:w-[560px]"
-							height="315"
+							className="rounded-lg w-[auto] md:w-[560px] h-[200px] md:h-[315px]"
 							src="https://www.youtube.com/embed/z8gtUPtji1c?si=OdH0gTN50_XElgyY"
 							title="YouTube video player"
 							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
