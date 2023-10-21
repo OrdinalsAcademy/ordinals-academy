93e7704dac6cc90f8d4d435a9888650e6c4a4dfc
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-10-21 06:20:14 +0200

    Update _document.tsx

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -30,7 +30,7 @@ class MyDocument extends Document<Props> {
 					<link
 						data-react-helmet="true"
 						rel="icon"
-						href="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-L9iS6Wm2hynS5H9Gj7j%2Favatar.png?generation=1523462254548780&amp;alt=media"
+						href="https://github.githubassets.com/favicons/favicon.png"
 					/>
 				</Head>
 				<body>
