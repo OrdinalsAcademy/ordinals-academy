56125dd51d17153e639ebc4f82aaeceb63f2003b
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-10-28 04:35:00 +0200

    Update Footer.tsx

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -8,32 +8,29 @@ export const Footer = () => {
 	const { t } = useTranslation('footer');
 
 	return (
-		<footer className="flex w-full text-white pt-20 pb-20 bg-customWhite dark:bg-customDark text-black dark:text-white">
+		<footer className="flex w-full text-white pt-12 pb-20 bg-customWhite dark:bg-customDark text-black dark:text-white">
 			<div className=" max-w-6xl container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-				<Link href="/">
-					<span className="cursor-pointer flex items-center font-display text-2xl">
-						<div className="flex items-center flex-shrink-0 text-white">
-							<span className="rounded-full p-1.5 text-xl tracking-tight font-Mitr text-customGray dark:text-white hover:text-customBitcoin dark:hover:text-customBitcoin">
-								Ordinals Academy
-							</span>
-						</div>
-					</span>
+				<Link
+					href="/"
+					className="cursor-pointer flex font-display text-2xl"
+				>
+					<div className="flexflex-shrink-0 text-white">
+						<span className="rounded-full p-1.5 text-xl tracking-tight font-Mitr text-customGray dark:text-white hover:text-customBitcoin dark:hover:text-customBitcoin">
+							Ordinals Academy
+						</span>
+					</div>
 				</Link>
 				<div>
 					<h3 className="font-bold mb-2">Discover</h3>
 					<ul>
 						<li className="mb-2">
-							<Link href="/articles">
-								<span className="hover:underline">
-									Articles
-								</span>
+							<Link href="/articles" className="hover:underline">
+								Articles
 							</Link>
 						</li>
 						<li className="mb-2">
-							<Link href="/glossary">
-								<span className="hover:underline">
-									Glossary
-								</span>
+							<Link href="/glossary" className="hover:underline">
+								Glossary
 							</Link>
 						</li>
 					</ul>
@@ -62,7 +59,7 @@ export const Footer = () => {
 				</div>
 
 				<div>
-					<h3 className="font-bold mb-2">Socials</h3>
+					<h3 className="font-bold mb-3">Socials</h3>
 					<div className="flex space-x-4">
 						<a
 							href="https://twitter.com/OrdinalsAcademy"
@@ -72,8 +69,8 @@ export const Footer = () => {
 							<Image
 								src={x.src}
 								alt="Twitter"
-								width={24}
-								height={24}
+								width={20}
+								height={20}
 							/>
 						</a>
 						<a
@@ -84,8 +81,8 @@ export const Footer = () => {
 							<Image
 								src={zealy.src}
 								alt="Zealy"
-								width={24}
-								height={24}
+								width={20}
+								height={20}
 							/>
 						</a>
 					</div>
